be44ccbc16f3b56afcd859380cb61d2b2ce2472a
yeahyouknowme <forrest.yeast@gmail.com>
2021-04-07 15:51:13 -0700

    comments

--- euler.py
+++ euler.py
@@ -1,21 +1,20 @@
 import numpy as np
 
 
-## FINAL RESULT IS SOMETIMES WRONG ???? ##
+## FINAL DIGITS SOMETIMES WRONG ???? ##
 def spigot(digit):
     
     # initialize array
     spigotArray = np.array([])
     for x in range(digit):
         spigotArray = np.append(spigotArray, int(1))
-    
+
+    # print digits of euler's number 
     for n in range(0, digit, 1):
-        ## SINGLE STEP THROUGH ALGHORITHM ##
         spigotArray = spigotArray * 10
-        # loop through array #
+        # loop through array 
         for i in range(digit-1, 0, -1):
             ans = divmod(spigotArray[i], (i+2))
-
             spigotArray[i] = int(ans[1])
             spigotArray[i-1] += int(ans[0])
         drip = int(spigotArray[0]) // 2
