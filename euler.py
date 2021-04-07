fc1ab18c7185e0a48646da61bda86d9d8b490466
yeahyouknowme <forrest.yeast@gmail.com>
2021-04-07 15:07:23 -0700

    first

---
+++ euler.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+## FINAL RESULT IS SOMETIMES WRONG ???? ##
+def spigot(digit):
+    
+    # initialize array
+    spigotArray = np.array([])
+    for x in range(digit):
+        spigotArray = np.append(spigotArray, int(1))
+    
+    for n in range(0, digit, 1):
+        ## SINGLE STEP THROUGH ALGHORITHM ##
+        spigotArray = spigotArray * 10
+        # loop through array #
+        print(spigotArray[0])
+        for i in range(digit-1, 0, -1):
+            ans = divmod(spigotArray[i], (i+2))
+
+            spigotArray[i] = int(ans[1])
+            spigotArray[i-1] += int(ans[0])
+        print(spigotArray[0])
+       # drip = int(spigotArray[0]) // 2
+        spigotArray[0] = spigotArray[0] % 2
+        #print(drip)
+
+
+        
+
+spigot(9)
+
+
+
